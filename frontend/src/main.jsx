9f0682ed49d12b617b384f217d9e392d0d91ef39
SachinMyadam <nanisachin45@gmail.com>
2026-06-22 19:22:31 +0530

    fix router rendering and vercel spa routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import "./index.css";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </AuthProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
